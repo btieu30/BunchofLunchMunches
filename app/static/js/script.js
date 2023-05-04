ac57c3bedcafde1a25c5301a719dbc249c04a4df
Vivian Teo <vivianteo950@gmail.com>
2023-05-04 14:06:43 -0400

    working on dropdowns

---
+++ app/static/js/script.js
@@ -0,0 +1,12 @@
+// Bunch of Lunch Munches
+// SoftDev pd8
+// ////2023
+// P04
+
+// const searchButton = document.getElementById('search-button');
+// const searchInput = document.getElementById('search-input');
+// searchButton.addEventListener('click', () => {
+//   const inputValue = searchInput.value;
+//   alert(inputValue);
+// });
+
