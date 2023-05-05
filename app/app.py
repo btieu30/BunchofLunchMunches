f14976b1587b8518a0d9df9a40e36c3cf881b655
Vivian Teo <vteo30@stuy.edu>
2023-05-05 00:03:27 -0400

    integrating jinja and working on displays, trying to figure out jinja+js tgt but not working for now, but app runs good

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, url_for
 app = Flask(__name__) 
 
 @app.route("/")
 def root():
-    return "No hablo queso!"
+    # return "No hablo queso!"
+    return render_template('home.html', n=3)
+
 
 if __name__ == "__main__":
     app.debug = True
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -10,3 +10,21 @@
 //   alert(inputValue);
 // });
 
+var n = 3;
+var disappearAllElse = function(stay) {
+    for (let i = 0; i < n; i++) {
+        var accordion = document.getElementById("accordion"+i);
+        if (i != stay) {
+            accordion.style.display="none";
+        }
+    }
+}
+
+// problem: js cant read jinja rn, so it can't get the button by id, which controlled using jinja
+// for (let i = 0; i < n; i++) {
+//     var buttonID = "button"+i;
+//     var buttonDrop = document.getElementById(buttonID);
+    // buttonDrop.addEventListener("click", disappearAllElse(i));
+// }
+
+
