4884a3c1aced0fbcf7a41dee919648288b7b1d8d
anthonyqsun <qsun30@stuy.edu>
2023-05-11 13:28:46 -0400

    add devlog

--- app/parse.py
+++ app/parse.py
@@ -7,9 +7,11 @@ db = database.Database()
 with open('DOHMH_New_York_Restaurant_Inspection_Results.csv') as csvFile:
     restaurants = csv.reader(csvFile)
 
+commonID=""
+
 for line in restaurants:
     #addRestaurant(self,  id,    name, borough,                address, zipCode,    desc,   date, violation,    score,    grade,      lat,     long)
-    db.addRestaurant(line[0], line[1], line[2], f"{line[3]} {line[4]}", line[5], line[7], line[8], line[11], line[13], line[14], line[18], line[19])
+    # db.addRestaurant(line[0], line[1], line[2], f"{line[3]} {line[4]}", line[5], line[7], line[8], line[10], line[13], line[14], line[18], line[19])
+    
 
-database.commit(database.self)
-database.c.close()
+db.close()
