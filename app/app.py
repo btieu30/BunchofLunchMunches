1d7e4474ab2d1bd8ddabfbb488dd43b3b57b2cc7
Brianna Tieu <btieu30@stuy.edu>
2023-05-24 03:15:55 -0400

    what key??

--- app/app.py
+++ app/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 db = Database()
 
+with open ("app/keys/key_goog.txt") as key:
+    key_goog = key.read().strip()
+
 @app.route("/", methods=['GET', 'POST'])
 def root():
     # restaurant name, grade, review | borough | building | street | zip code |  cuisine description | inspection date | violation code | 
@@ -15,7 +18,7 @@ def root():
         restaurants=db.getRestaurants(parseSearchRequest(request.form['query']))
         returntext=request.form['query']
 
-    return render_template('home.html', restList=restaurants, returntext=returntext)
+    return render_template('home.html', restList=restaurants, returntext=returntext, key_goog = key_goog)
 
 @app.route("/saved", methods=['GET', 'POST'])
 def saved_page():
@@ -23,7 +26,7 @@ def saved_page():
     restaurants = db.getRestaurants()
 
 
-    return render_template('saved.html', restList=restaurants)
+    return render_template('saved.html', restList=restaurants, key_goog = key_goog)
 
 
 if __name__ == "__main__":
